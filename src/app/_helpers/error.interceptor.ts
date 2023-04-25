8b8af17a2f52ac11e13605872ad078f1e5d958d6
Per Steen Olsen <persteen22@hotmail.com>
2023-04-25 09:23:27 +0200

    Adjusting the http error handling

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -7,30 +7,41 @@ import { AuthenticationService } from '@/_services';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            
+	constructor(private authenticationService: AuthenticationService) { }
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+		return next.handle(request).pipe(catchError(err => {
+
+			// NOT AUTORIZED
+			// Auto logout if 401 response returned from api
 			if (err.status === 401) {
-                // Auto logout if 401 response returned from api
-                this.authenticationService.logout();
-                location.reload(true);
-            }
-            			
-            // For testing !			
+				this.authenticationService.logout();
+
+				//location.reload(true);
+				location.reload();
+			}
+
+			// Testing the errors from the Web API !			
 			//alert( 'Error: ' + JSON.stringify( err ));
 			//alert( 'Status 1: ' + err.status + ' Status 2: ' + err.statusText + ' Message 1: ' + err.message +  ' Message 2: ' + err.error.message );
-			
-			// Look for error msg defined at the Model Anotation in Web API => else look for error messages => else look for http status code 
-			// Note: A wrong Web API connection - 404 can only be catch by "err.message"
-			var error = '';
+
+			// Default - for differents status codes than 400 or 404 look for http status code
+			var error = err.status;
+
+			// NOT FOUND
+			// A wrong api connection - 404 can only be catch by "err.message"
 			if (err.status === 404)
 				error = err.message;
-			else
-			    error = err.error.errors || err.error.message || err.status; 
-					   			
-            return throwError(error);
-        }))
-    }
+
+			// BAD REQUEST	
+			// 1) Look for error msg defined at the Model Anotation in api - like Post.cs
+			// 2) Look for error messages defined at the api controller / service like User email already taken 
+			// 3) Look for http status code 
+			if (err.status === 400)
+				error = err.error.errors || err.error.message || err.status;
+
+			return throwError(error);
+		}))
+	}
 }
